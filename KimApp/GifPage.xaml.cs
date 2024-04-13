984bf9a957b0d9c1acc4284a339c5a9bc056690c
Kim van Rijswijk <kim.van@etu.unilim.fr>
2024-04-13 21:21:45 +0200

    ajout de la structure de la page d'accueil

---
+++ KimApp/GifPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+
+namespace KimApp
+{
+    public partial class GifPage : ContentPage
+    {
+        public GifPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnBackButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
