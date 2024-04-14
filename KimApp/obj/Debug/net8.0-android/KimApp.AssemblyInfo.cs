612e0ea1f1dc2928eb486a1d0af32e20093db48c
Kim van Rijswijk <kim.van@etu.unilim.fr>
2024-04-14 20:57:58 +0200

    Modification du splash screen

--- KimApp/obj/Debug/net8.0-android/KimApp.AssemblyInfo.cs
+++ KimApp/obj/Debug/net8.0-android/KimApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KimApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f40d942feb0e3d6e89486a2c227dcf8ac3f59d97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0bc8c87d3af871fe3408bf313653803f461f69d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("KimApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KimApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
